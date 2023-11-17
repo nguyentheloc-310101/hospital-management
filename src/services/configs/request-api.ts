4d4339be34f7d215d2a593d8e641e1a0c620d3b5
loc-dev <nguyentheloc310101@gmail.com>
2023-11-17 13:11:27 +0700

    feat(antd registry): set up layout

--- src/services/configs/request-api.ts
+++ src/services/configs/request-api.ts
@@ -1,2 +1,18 @@
-const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
-export { apiEndpoint };
+import axios, { AxiosRequestConfig } from 'axios';
+import { apiEndpoint } from './endpoint';
+
+const requestApi = ({ method = 'GET', ...config }: AxiosRequestConfig) => {
+  const token = JSON.parse(localStorage.getItem('auth.token') ?? '{}');
+  return axios({
+    method,
+    baseURL: apiEndpoint,
+    ...config,
+    headers: {
+      ...config.headers,
+      Authorization: `Bearer ${token?.accessToken}`,
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+};
+
+export default requestApi;
---
+++ src/utils/theme/theme-config.ts
@@ -0,0 +1,11 @@
+import type { ThemeConfig } from 'antd';
+
+const themeConfig: ThemeConfig = {
+  token: {
+    // borderRadius: 8,
+    colorPrimary: '#3370FF',
+  },
+  components: {},
+};
+
+export default themeConfig;
