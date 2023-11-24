006442d39ae873c808a9a14ac264d032c1a052db
khangqn12345 <125542949+khangqn12345@users.noreply.github.com>
2023-11-24 14:37:00 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { supabase } from '@/services/supabase/supabase-client';
-import { log } from 'console';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -8,7 +7,7 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
-
+  const [authenticated,setAuthenticated]= useState(false);
   const handleSignUp = async () => {
     await supabase.auth.signUp({
       email,
@@ -21,11 +20,18 @@ export default function Login() {
   };
 
   const handleSignIn = async () => {
-    await supabase.auth.signInWithPassword({
+    const{data,error} = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-    router.refresh();
+    console.log(data);
+    // console.log(error);
+    if(data.user!==null && data.session!==null){
+      router.push('/dashboard/treatments')
+    }
+    else{
+      setAuthenticated(true)
+    }
   };
 
   const handleSignOut = async () => {
@@ -34,7 +40,7 @@ export default function Login() {
   };
 
   return (
-    <>
+    <div className='w-full h-[100vh] flex items-center justify-center'>
       <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
@@ -52,15 +58,15 @@ export default function Login() {
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" id="password">
               Password
-              <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              <input className="shadow appearance-none border border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password" 
                 placeholder="Password" 
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {setPassword(e.target.value),setAuthenticated(false)}}
              />
+             {password.length===0 && <p className="text-red-500 text-xs italic">Please fill the password.</p>}
+             {authenticated && <p className="text-red-500 text-xs italic">Invalid password.</p>}
             </label>
-            
-            {password.length===0 &&<p className="text-red-500 text-xs italic">Please fill the password.</p>}
           </div>
           <div className="flex items-center justify-between">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSignIn}>
@@ -75,8 +81,6 @@ export default function Login() {
           &copy;2020 Acme Corp. All rights reserved.
         </p>
       </div>
-
-    </>
+    </div>
   );
 }
-
