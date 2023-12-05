dc3dbb195c8bfb74a173beb33d55768a196a0a4d
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2023-12-05 19:59:21 +0700

    feat(employeeTable): insert rows to database

--- src/app/dashboard/employees/AddEmployeeModal.tsx
+++ src/app/dashboard/employees/AddEmployeeModal.tsx
@@ -12,15 +12,15 @@ import { useSearchParams } from 'next/navigation';
 import { supabase } from '@/services/supabase/supabase-client';
 const { Title } = Typography;
 //TODO: fix -> gom vao props
-  //TODO: lay het record tu supabase ve xong roi console.log ra.
+//TODO: lay het record tu supabase ve xong roi console.log ra.
 
-  //TODO: tao zustand (store) luu cac thong tin employee fetch ve./
+//TODO: tao zustand (store) luu cac thong tin employee fetch ve./
 
 
 const AddEmployeeModal = () => {
 
   const [id, setID] = React.useState('');
-  
+
   const [fname, setFName] = React.useState('');
   const [lname, setLName] = React.useState('');
   const [birthdate, setBirthdate] = React.useState<any | null>(null);
@@ -32,7 +32,7 @@ const AddEmployeeModal = () => {
   const [degreeYear, setDegreeYear] = React.useState<any | null>(null);
   const [role, setRole] = React.useState<any | null>(null);
   const [department, setDepartment] = React.useState<any | null>(null);
-  const [insertError,setInsertError] = React.useState<any|null>(null);
+  const [insertError, setInsertError] = React.useState<any | null>(null);
   const genders: MenuProps['items'] = [
     {
       label: 'Male',
@@ -116,34 +116,58 @@ const AddEmployeeModal = () => {
 
   const [modalVisible, setModalVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const handleSubmit = async (e:any)=>
-  {
-     
+  const handleSubmit = async () => {
 
-      if(!fname || !id) //in case of inserting null to non null
-      {
-          setInsertError("First name or id cannot be set blank")
-          console.log(insertError)
-          return
-      }
-      
-      const {data, error} = await supabase
-      .from('emeployee')
-      .update({fname,lname,birthdate,gender,address,degree,degreeYear,startDate,role, department})
+
+    if (!fname || !id) //in case of inserting null to non null
+    {
+      setInsertError("First name or id cannot be set blank")
+      console.log(insertError)
+      return
+    }
+
+
+    const { data, error } = await supabase
+      .from('employee')
+      .insert([
+        {
+          UniqueCode: id,
+          FName: fname,
+          LName: lname,
+          Dob: birthdate,
+          Gender: gender,
+          Address: address,
+          DegreeName: degree,
+          DegreeYear: degreeYear,
+          StartDate: startDate,
+          Role: role,
+          DeptCode: department
+        },
+        // { FName: fname },
+        // { LName: lname},
+        // { Dob: birthdate},
+        // { Gender: gender},
+        // { Address: address},
+        // { DegreeName:degree},
+        // { DegreeYear:degreeYear},
+        // { StartDate: startDate},
+        // { Role: role},
+        // { DeptCode: department}
+
+      ])
       .select()
+    console.log(data)
 
-      if(error)
-      {
-        console.log(error)
-        setInsertError(error)
+    if (error) {
+      console.log(error)
+      setInsertError(error)
 
-      }
-      if(data)
-      {
-          console.log(data)
-          setInsertError(null)
-          handleCancel()
-      }
+    }
+    if (data) {
+      console.log(data)
+      setInsertError(null)
+      handleCancel()
+    }
 
   }
   const handleOk = () => {
@@ -151,11 +175,11 @@ const AddEmployeeModal = () => {
     setTimeout(() => {
       setModalVisible(false);
       setConfirmLoading(false);
-      handleSubmit("")
+      handleSubmit()
       // This is where to handle the adding states to the database.
     }, 2000);
-    
-  
+
+
   };
   const showModal = () => {
     setModalVisible(true);
@@ -168,116 +192,116 @@ const AddEmployeeModal = () => {
   return (
     <>
       <Button
-      
+
         type="primary"
         onClick={showModal}
-        
+
       >
-        {<UserAddOutlined/>} Add Employee
+        {<UserAddOutlined />} Add Employee
       </Button>
-    <Modal
-      title={'Add employee'}
-      open={modalVisible}
-      onCancel={handleCancel}
-      onOk={handleOk}
-      okText={'Add'}
-      okButtonProps={{ style: { backgroundColor: '#16ABF2' } }}
-      confirmLoading={confirmLoading}>
-        
-      <Title level={5}>ID</Title> 
-      {
-        insertError && (
-          <Alert message={insertError} type='error'></Alert>
-        )
-      }
-      <Input
-        value={id}
-        onChange={(e) => setID(e.target.value)}
-        placeholder={'Enter ID'}
-      /> 
-      
-      <Title level={5}>First Name</Title>
-      <Input
-        value={fname}
-        onChange={(e) => setFName(e.target.value)}
-        placeholder={'Enter first name'}
-      />
-      <Title level={5}>Last Name</Title>
-      <Input
-        value={lname}
-        onChange={(e) => setLName(e.target.value)}
-        placeholder={'Enter last name'}
-      />
-      <Title level={5}>Birthdate</Title>
-      <DatePicker
-        size={'large'}
-        value={birthdate}
-        onChange={(date) => {
-          setBirthdate(date);
-        }}></DatePicker>
-      <Title level={5}>Gender</Title>
+      <Modal
+        title={'Add employee'}
+        open={modalVisible}
+        onCancel={handleCancel}
+        onOk={handleOk}
+        okText={'Add'}
+        okButtonProps={{ style: { backgroundColor: '#16ABF2' } }}
+        confirmLoading={confirmLoading}>
 
-      <Dropdown.Button
-        dropdownRender={()=>(<Menu items={genders} onClick={handleGenderClick}></Menu>)}
-        trigger={['click']}
-        icon={<DownOutlined />}>
-        {gender === null ? 'Not Stated' : gender || 'Select Gender'}
-      </Dropdown.Button>
+        <Title level={5}>ID</Title>
+        {
+          insertError && (
+            <Alert message={insertError} type='error'></Alert>
+          )
+        }
+        <Input
+          value={id}
+          onChange={(e) => setID(e.target.value)}
+          placeholder={'Enter ID'}
+        />
 
-      <Title level={5}>Address</Title>
-      <Input
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        placeholder={'Enter address'}
-      />
-      <Title level={5}>Phone</Title>
-      <Input
-        placeholder={'Enter phone number'}
-        value={phoneNumber}
-        onChange={(phoneNumber) => {
-          setPhoneNumber(phoneNumber.target.value);
-        }}></Input>
+        <Title level={5}>First Name</Title>
+        <Input
+          value={fname}
+          onChange={(e) => setFName(e.target.value)}
+          placeholder={'Enter first name'}
+        />
+        <Title level={5}>Last Name</Title>
+        <Input
+          value={lname}
+          onChange={(e) => setLName(e.target.value)}
+          placeholder={'Enter last name'}
+        />
+        <Title level={5}>Birthdate</Title>
+        <DatePicker
+          size={'large'}
+          value={birthdate}
+          onChange={(date) => {
+            setBirthdate(date);
+          }}></DatePicker>
+        <Title level={5}>Gender</Title>
 
-      <Title level={5}>Degree</Title>
-      <Input
-        value={degree}
-        onChange={(e) => setDegree(e.target.value)}
-        placeholder={'Enter degree name'}
-      />
-      <Title level={5}>Degree Year</Title>
-      <Input
-        value={degreeYear}
-        onChange={(e) => setDegreeYear(e.target.value)}
-        placeholder={'Enter degree year'}
-      />
-      <Title level={5}>Start Date</Title>
-      <DatePicker
-        size={'large'}
-        value={startDate}
-        onChange={(date) => {
-          setStartDate(date);
-        }}></DatePicker>
-      <Title level={5}>Role</Title>
+        <Dropdown.Button
+          dropdownRender={() => (<Menu items={genders} onClick={handleGenderClick}></Menu>)}
+          trigger={['click']}
+          icon={<DownOutlined />}>
+          {gender === null ? 'Not Stated' : gender || 'Select Gender'}
+        </Dropdown.Button>
 
-      <Dropdown.Button
-                
-                dropdownRender={()=>(<Menu items={roles} onClick={handleRolesClick}/>)}     
-                trigger={['click']}
-                icon={<DownOutlined/>}
-            >
-                {role === null ? 'Not Stated' : role || 'Select Role'}
-            </Dropdown.Button>
+        <Title level={5}>Address</Title>
+        <Input
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder={'Enter address'}
+        />
+        <Title level={5}>Phone</Title>
+        <Input
+          placeholder={'Enter phone number'}
+          value={phoneNumber}
+          onChange={(phoneNumber) => {
+            setPhoneNumber(phoneNumber.target.value);
+          }}></Input>
 
-      <Title level={5}>Department Name</Title>
-      <Dropdown.Button
-                dropdownRender={()=>(<Menu items={departments} onClick={handleDeparmentClick}></Menu>)}
-                
-                trigger={['click']}
-                icon={<DownOutlined/>}
-            >
-                {department === null ? 'Not Stated' : department || 'Select Department'}
-            </Dropdown.Button>
-    </Modal>
+        <Title level={5}>Degree</Title>
+        <Input
+          value={degree}
+          onChange={(e) => setDegree(e.target.value)}
+          placeholder={'Enter degree name'}
+        />
+        <Title level={5}>Degree Year</Title>
+        <Input
+          value={degreeYear}
+          onChange={(e) => setDegreeYear(e.target.value)}
+          placeholder={'Enter degree year'}
+        />
+        <Title level={5}>Start Date</Title>
+        <DatePicker
+          size={'large'}
+          value={startDate}
+          onChange={(date) => {
+            setStartDate(date);
+          }}></DatePicker>
+        <Title level={5}>Role</Title>
+
+        <Dropdown.Button
+
+          dropdownRender={() => (<Menu items={roles} onClick={handleRolesClick} />)}
+          trigger={['click']}
+          icon={<DownOutlined />}
+        >
+          {role === null ? 'Not Stated' : role || 'Select Role'}
+        </Dropdown.Button>
+
+        <Title level={5}>Department Name</Title>
+        <Dropdown.Button
+          dropdownRender={() => (<Menu items={departments} onClick={handleDeparmentClick}></Menu>)}
+
+          trigger={['click']}
+          icon={<DownOutlined />}
+        >
+          {department === null ? 'Not Stated' : department || 'Select Department'}
+        </Dropdown.Button>
+      </Modal>
     </>
   );
 };
--- src/app/dashboard/employees/EmployeeTable.tsx
+++ src/app/dashboard/employees/EmployeeTable.tsx
@@ -3,79 +3,87 @@ import { Table } from 'antd';
 import { supabase } from '@/services/supabase/supabase-client';
 import { error } from 'console';
 import { Alert } from 'antd';
+import { CustomTable } from '@/components/common/table/TableCustom';
 const columns = [
   {
     title: 'Unique Code',
     dataIndex: 'UniqueCode',
     key: 'UniqueCode',
+    
     sorter: (a:any, b:any) => a.UniqueCode.localeCompare(b.UniqueCode),
   },
   {
     title: 'First Name',
     dataIndex: 'FName',
     key: 'FName',
+    
     sorter: (a:any, b:any)=> a.FName.localeCompare(b.FName),
   },
   {
     title: 'Last Name',
     dataIndex: 'LName',
     key: 'LName',
-    sorter: (a:any, b:any)=> a.LName.localeCompare(b.LName),
+    sorter: (a:any, b:any)=>{ 
+      
+      
+      a.LName?.localeCompare(b.LName)
+    
+    },
   },
   {
     title: 'DOB',
     dataIndex: 'Dob',
     key: 'Dob',
-    sorter: (a:any, b:any)=> a.Dob.localeCompare(b.Dob),
+    sorter: (a:any, b:any)=> a.Dob?.localeCompare(b.Dob),
   },
   {
     title: 'Gender',
     dataIndex: 'Gender',
     key: 'Gender',
-    sorter: (a:any, b:any)=> a.Gender.localeCompare(b.Gender),
+    sorter: (a:any, b:any)=> a.Gender?.localeCompare(b.Gender),
   },
   {
     title: 'Address',
     dataIndex: 'Address',
     key: 'Address',
-    sorter: (a:any, b:any)=> a.Address.localeCompare(b.Address),
+    sorter: (a:any, b:any)=> a.Address?.localeCompare(b.Address),
   },
   {
     title: 'Degree Name',
     dataIndex: 'DegreeName',
     key: 'DegreeName',
-    sorter: (a:any, b:any)=> a.DegreeName.localeCompare(b.DegreeName),
+    sorter: (a:any, b:any)=> a.DegreeName?.localeCompare(b.DegreeName),
   },
   {
     title: 'Degree Year',
     dataIndex: 'DegreeYear',
     key: 'DegreeYear',
-    sorter: (a:any, b:any)=> a.DegreeYear.localeCompare(b.DegreeYear),
+    sorter: (a:any, b:any)=> a.DegreeYear?.localeCompare(b.DegreeYear),
   },
   {
     title: 'Start Date',
     dataIndex: 'StartDate',
     key: 'StartDate',
-    sorter: (a:any, b:any)=> a.StartDate.localeCompare(b.StartDate),
+    sorter: (a:any, b:any)=> a.StartDate?.localeCompare(b.StartDate),
   },
   {
     title: 'Role',
     dataIndex: 'Role',
     key: 'Role',
-    sorter: (a:any, b:any)=> a.Role.localeCompare(b.Role),
+    sorter: (a:any, b:any)=> a.Role?.localeCompare(b.Role),
   },
   {
     title: 'Dept Code',
     dataIndex: 'DeptCode',
     key: 'DeptCode',
-    sorter: (a:any, b:any)=> a.DeptCode.localeCompare(b.DeptCode),
+    sorter: (a:any, b:any)=> a.DeptCode?.localeCompare(b.DeptCode),
   },
-  {
-    title: 'Department',
-    dataIndex: 'departmentTitleColumn', // ! i was trying to fetch the department's name, but it didnt work
-    key: 'Title',
-    // sorter: (a:any, b:any)=> a.Title.localeCompare(b.Title),
-  },
+  // {
+  //   title: 'Department',
+  //   dataIndex: 'Title', // ! i was trying to fetch the department's name, but it didnt work
+  //   key: 'Title',
+  //   // sorter: (a:any, b:any)=> a.Title.localeCompare(b.Title),
+  // },
 
 ];
 
@@ -114,7 +122,9 @@ const EmployeeTable = () => {
     fetchData();
   }, []); 
 
-  
+  const fixedColumn = columns.slice(0,1)
+  const scrollColumn = columns.slice(1) //start from index 1
+
   return (
     <>
       {/* <Table
@@ -134,7 +144,13 @@ const EmployeeTable = () => {
         columns={columns}
         dataSource={data}
       /> */}
-      <Table dataSource={data} columns={columns}></Table>
+      <Table dataSource={data} 
+      columns={columns}
+
+      
+      scroll={{ x: true }}
+      ></Table>
+      
       {
         fetchError && (
           <Alert message={fetchError} type='error'></Alert> //? conditional rendering? 
