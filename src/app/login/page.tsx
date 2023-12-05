5b1621493534e0c362bb268e21867d04eb13991e
khangqn12345 <125542949+khangqn12345@users.noreply.github.com>
2023-12-05 09:24:39 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -72,9 +72,9 @@ export default function Login() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSignIn}>
               Sign In
             </button>
-            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            <li className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
               Forgot Password?
-            </a>
+            </li>
           </div>
         </form>
         <p className="text-center text-gray-500 text-xs">
